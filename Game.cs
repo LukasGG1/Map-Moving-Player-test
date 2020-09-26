c3af5ea06f09a58ac7368cbf20394b51e7ac409f
Lukas Gresham <s208045@students.aie.edu>
2020-09-26 15:21:45 -0500

    Map

--- Game.cs
+++ Game.cs
@@ -4,12 +4,42 @@ using System.Text;
 
 namespace HelloWorld
 {
+
     class Game
     {
+        bool gameOver = false;
         //Run the game
         public void Run()
         {
-            
+            Start();
+            RequestName();
+            while (gameOver == false)
+            {
+                Update();
+            }
+
+            End();
+        }
+
+        void RequestName()
+        //body
+        {
+            //Initialize Order 34
+            char input = ' ';
+            while (input != '1')
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome! What is your name, stranger?");
+                _playerName = Console.ReadLine();
+                Console.WriteLine("Greeting, " + _playerName);
+                input = GetInput("Yes", "No", "Is this name you want " + _playerName + "?");
+                if (input == '2')
+                {
+                    Console.WriteLine("Yeah, You're right. It is not good name for you.");
+
+                }
+            }
+
         }
 
         //Performed once when the game begins
